43b76603acbcc16372e88e12cf459a85c58eb43d
Mike Jerred <mjerred.work@gmail.com>
2024-03-06 23:29:10 +0000

    refactor(overview): use signals

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -1,22 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormControl } from '@angular/forms';
 import { MatDividerModule } from '@angular/material/divider';
-import { Select, Store } from '@ngxs/store';
+import { MatSliderModule } from '@angular/material/slider';
+import { Store } from '@ngxs/store';
 import moment, { Moment } from 'moment';
-import { Observable, combineLatest, last, map } from 'rxjs';
-import { mapRecord } from '~/core';
+import { combineLatest, map } from 'rxjs';
 
-import { CoreModule } from '~/core';
+import { CoreModule, mapRecord } from '~/core';
 import { AssetsState } from '~/state/clients/assets.state';
 import { PeopleState } from '~/state/clients/people.state';
 import { StagesState } from '~/state/clients/plans/stages.state';
-import { DynamicWithdrawalScheme, Portfolio, Stage, UnrolledStage, WithdrawalScheme } from '~/state/clients/plans/stages.state.model';
-import { Graph, GraphsState } from '~/state/graphs.state';
+import { DynamicWithdrawalScheme, Portfolio, UnrolledStage } from '~/state/clients/plans/stages.state.model';
+import { GraphsState } from '~/state/graphs.state';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
   imports: [
     MatDividerModule,
+    MatSliderModule,
 
     CoreModule
   ],
@@ -24,218 +27,188 @@ import { Graph, GraphsState } from '~/state/graphs.state';
   styleUrl: './overview.component.scss'
 })
 export class OverviewComponent {
-  // cryptoAmount = this.store.select(InvestmentsState.crypto).pipe(
-  //   map(crypto => crypto.map(investment => investment.balanceGbp)),
-  //   map(amounts => amounts.reduce((amount, total) => amount + total, 0))
-  // );
+  store = inject(Store);
 
-  // netWorth = combineLatest([
-  //   this.cryptoAmount
-  // ]).pipe(
-  //   map(amounts => amounts.reduce((amount, total) => amount + total, 0))
-  // );
+  percentileControl = new FormControl(5, { nonNullable: true });
 
-  // cryptoPercentage = combineLatest([this.cryptoAmount, this.netWorth]).pipe(
-  //   map(([amount, total]) => 100 * amount / total)
-  // );
+  percentile = toSignal(this.percentileControl.valueChanges, { initialValue: this.percentileControl.value });
+  maxYear = toSignal(this.store.select(PeopleState.maxYear(3)), { requireSync: true });
+  people = toSignal(this.store.select(PeopleState.people), { requireSync: true });
+  graphs = toSignal(
+    combineLatest([
+      this.store.select(GraphsState.inflation),
+      this.store.select(GraphsState.cash),
+      this.store.select(GraphsState.bonds),
+      this.store.select(GraphsState.stocks),
+      this.store.select(GraphsState.crypto)
+    ]).pipe(map(([[, inflation], [, cash], [, bonds], [, stocks], [, crypto]]) => ({ inflation, cash, bonds, stocks, crypto }))),
+    { requireSync: true }
+  );
+  stages = toSignal(this.store.select(StagesState.unrolledStages), { requireSync: true });
+  // toSignal(store.select(AssetsState.portfolioTotals), { requireSync: true })
+  initialPortfolio = signal({
+    cash: 0,
+    bonds: 0,
+    stocks: 595000,
+    crypto: 0
+  });
 
-  // @Select(StrategyState.stages)
-  // stages!: Observable<Stage[]>;
+  yearsPerCycle = computed(() => this.maxYear() - moment().year());
+  monthsPerCycle = computed(() => this.yearsPerCycle() * 12);
+  initialTotal = computed(() => Object.values(this.initialPortfolio()).reduce((total, value) => total + value, 0));
 
-  // @Select(PeopleState.people)
-  // people!: Observable<Person[]>;
-
-  standardDeviations = 3;
-
-  xOffset = 0;
-  xUnit = 300 / (12 * 1);
-  xCount = 13;
-  xLabelWidth = '10%';
-  yUnit = 1;
-
-  medianPath = signal<string>('');
-  percentilesPath = signal<string>('');
-  path = signal<string>('');
-  xLabels = signal<Record<string, string[]>>({});
-  yLabels = signal<{ value: number; y: string; }[]>([]);
-  successRate = signal(0);
-
-  percentile = 5;
-
-  constructor(store: Store) {
-    // maybe show multiple plans on one graph in different colors
+  xUnit = computed(() => 300 / this.monthsPerCycle());
+  xInterval = computed(() => Math.floor(this.yearsPerCycle() / (this.xCount - 1)));
+  xLabels = computed(() => {
     const start = moment();
-    const maxYear = store.selectSnapshot(PeopleState.maxYear(this.standardDeviations));
-    const end = moment(`${maxYear}-01-01`);
-    const yearsPerCycle = maxYear - start.year();
-    const monthsPerCycle = yearsPerCycle * 12;
-
-    this.xUnit = 300 / monthsPerCycle;
-    const xInterval = Math.floor(yearsPerCycle / (this.xCount - 1));
-    const xRemainder = yearsPerCycle - xInterval * (this.xCount - 1);
-    const labelledXPortion = (yearsPerCycle - xRemainder) / yearsPerCycle;
-    this.xLabelWidth = `${100 * labelledXPortion / (this.xCount - 1)}%`;
-
     const getYears = (dateOfBirth: Moment) => {
       const age = Math.floor(start.diff(dateOfBirth, 'years'));
-      const allAges = [...Array(yearsPerCycle)].map((_, index) => `${age + index}`);
-      return allAges.filter((_, index) => index % xInterval == 0);
+      const allAges = [...Array(this.yearsPerCycle())].map((_, index) => `${age + index}`);
+      return allAges.filter((_, index) => index % this.xInterval() == 0);
     };
 
-    this.xLabels.set(Object.fromEntries(store.selectSnapshot(PeopleState.people).map(person => [
+    return Object.fromEntries(this.people().map(person => [
       person.name,
       getYears(person.dateOfBirth)
-    ])));
+    ]));
+  });
+  xLabelWidth = computed(() => {
+    const xRemainder = this.yearsPerCycle() - this.xInterval() * (this.xCount - 1);
+    const labelledXPortion = (this.yearsPerCycle() - xRemainder) / this.yearsPerCycle();
+    return `${100 * labelledXPortion / (this.xCount - 1)}%`;
+  });
+  yMaxValue = computed(() => {
+    const upper = [...this.points().upper].sort((a, b) => b - a);
 
-    const [, inflationGraph] = store.selectSnapshot(GraphsState.inflation);
-    const [, cashGraph] = store.selectSnapshot(GraphsState.cash);
-    const [, bondsGraph] = store.selectSnapshot(GraphsState.bonds);
-    const [, stocksGraph] = store.selectSnapshot(GraphsState.stocks);
-    const [, cryptoGraph] = store.selectSnapshot(GraphsState.crypto);
+    return Math.max(
+      Math.min(
+        upper[Math.floor((upper.length - 1) * 0.25)],
+        this.initialTotal() * 5
+      ),
+      this.initialTotal() * 1.5
+    );
+  });
+  yUnit = computed(() => 100 / this.yMaxValue());
+  yInterval = computed(() =>
+    this.yMaxValue() > 5000000000 ? 1000000000
+    : this.yMaxValue() > 2500000000 ? 500000000
+    : this.yMaxValue() > 1000000000 ? 250000000
+    : this.yMaxValue() > 500000000 ? 100000000
+    : this.yMaxValue() > 250000000 ? 50000000
+    : this.yMaxValue() > 100000000 ? 25000000
+    : this.yMaxValue() > 50000000 ? 10000000
+    : this.yMaxValue() > 25000000 ? 5000000
+    : this.yMaxValue() > 10000000 ? 2500000
+    : this.yMaxValue() > 5000000 ? 1000000
+    : this.yMaxValue() > 2500000 ? 500000
+    : this.yMaxValue() > 1000000 ? 250000
+    : this.yMaxValue() > 500000 ? 100000
+    : this.yMaxValue() > 250000 ? 50000
+    : this.yMaxValue() > 100000 ? 25000
+    : this.yMaxValue() > 50000 ? 10000
+    : this.yMaxValue() > 25000 ? 5000
+    : this.yMaxValue() > 10000 ? 2500
+    : this.yMaxValue() > 5000 ? 1000
+    : this.yMaxValue() > 2500 ? 500
+    : this.yMaxValue() > 1000 ? 250
+    : this.yMaxValue() > 500 ? 100
+    : this.yMaxValue() > 250 ? 50
+    : this.yMaxValue() > 100 ? 25
+    : this.yMaxValue() > 50 ? 10
+    : this.yMaxValue() > 25 ? 5
+    : 1
+  );
+  yLabels = computed(() => {
+    const yCount = Math.floor(this.yMaxValue() / this.yInterval());
+    const remainder = this.yMaxValue() - yCount * this.yInterval();
+    const labelledYPortion = (this.yMaxValue() - remainder) / this.yMaxValue();
+    const yLabelHeight = labelledYPortion / yCount;
+    return [...Array(yCount)].map((_, index) => ({
+      value: (index + 1) * this.yInterval(),
+      y: `${100 * (index + 1) * yLabelHeight}%`
+    }));
+  });
 
-    let monthsAvailable = Math.min(...[inflationGraph, cashGraph, bondsGraph, stocksGraph, cryptoGraph].map(graph => graph.data.length));
+  cycles = computed(() => {
+    const { inflation, cash, bonds, stocks, crypto } = this.graphs();
+    let monthsAvailable = Math.min(...[inflation, cash, bonds, stocks, crypto].map(graph => graph.data.length));
 
-    if (monthsPerCycle > monthsAvailable)
+    if (this.monthsPerCycle() > monthsAvailable)
       throw new Error(`Graphs do not provide enough data`);
 
-    const trimmedGraphData = [cashGraph, bondsGraph, stocksGraph, cryptoGraph].map(graph => graph.data.slice(-monthsAvailable));
+    const trimmedGraphData = [cash, bonds, stocks, crypto].map(graph => graph.data.slice(-monthsAvailable));
     const allGraphDeltas: Portfolio[] = [];
     const allInflationDeltas: number[] = [];
     for (let i = 0; i < monthsAvailable; ++i) {
       const [cash, bonds, stocks, crypto] = trimmedGraphData.map(data => data[i].value);
       allGraphDeltas.push({ cash, bonds, stocks, crypto });
-      allInflationDeltas.push(inflationGraph.data[i].value);
+      allInflationDeltas.push(inflation.data[i].value);
     }
 
-    const initialPortfolio = {
-      cash: 0,
-      bonds: 0,
-      stocks: 595000,
-      crypto: 0
-    }; //store.selectSnapshot(AssetsState.portfolioTotals);
-    const stages = store.selectSnapshot(StagesState.unrolledStages);
+    const start = moment();
+    const end = moment(`${this.maxYear()}-01-01`);
     const cycles: number[][] = [];
-    for (let m = 0; m < monthsAvailable - monthsPerCycle; ++m) {
-      const graphDeltas = allGraphDeltas.slice(m, m + monthsPerCycle);
-      const inflationDeltas = allInflationDeltas.slice(m, m + monthsPerCycle);
+    for (let m = 0; m < monthsAvailable - this.monthsPerCycle(); ++m) {
+      const graphDeltas = allGraphDeltas.slice(m, m + this.monthsPerCycle());
+      const inflationDeltas = allInflationDeltas.slice(m, m + this.monthsPerCycle());
       const cycle = this.calculateCycle(
         start,
         end,
-        initialPortfolio,
-        stages,
+        this.initialPortfolio(),
+        this.stages(),
         graphDeltas,
         inflationDeltas
       );
       cycles.push(cycle);
     }
 
+    return cycles;
+  });
+
+  successRate = computed(() => {
+    const cycles = this.cycles();
+    const successes = cycles.filter(cycle => cycle[cycle.length - 1] > 0);
+    return successes.length / cycles.length;
+  });
+
+  points = computed(() => {
+    const cycles = this.cycles();
+
     const lower: number[] = [];
-    const lowerPercentile: number[] = [];
     const median: number[] = [];
-    const upperPercentile: number[] = [];
     const upper: number[] = [];
-    for (let m = 0; m < monthsPerCycle; ++m) {
+    for (let m = 0; m < this.monthsPerCycle(); ++m) {
       const values = cycles.map(cycle => cycle[m]).sort((a, b) => a - b);
       const maxIndex = values.length - 1;
-      lower.push(values[0]);
-      lowerPercentile.push(values[Math.floor(maxIndex * (this.percentile / 100))]);
-      median.push(values[Math.floor(maxIndex / 2)]);
-      upperPercentile.push(values[Math.floor(maxIndex * (1 - this.percentile / 100))]);
-      upper.push(values[maxIndex]);
+      lower.push(values[Math.floor(maxIndex * (this.percentile() / 100))]);
+      median.push(values[Math.floor(maxIndex * 0.5)]);
+      upper.push(values[Math.floor(maxIndex * (1 - this.percentile() / 100))]);
     }
 
-    const finalValues = cycles.map(cycle => cycle[cycle.length - 1]);
-    const successes = finalValues.filter(value => value > 0);
-    this.successRate.set(successes.length / finalValues.length);
+    return { lower, median, upper };
+  });
 
-    const initialTotal = Object.values(initialPortfolio).reduce((total, value) => total + value, 0);
+  paths = computed(() => {
+    const { lower, median, upper } = this.points();
 
-    const maxVisibleAmount = Math.max(
-      Math.min(
-        [...upper].sort((a, b) => b - a)[Math.floor((upper.length - 1) * 0.25)],
-        initialTotal * 5
-      ),
-      initialTotal * 1.5
-    );
-    this.yUnit = 100 / maxVisibleAmount;
+    return {
+      median: [
+        `M ${this.getXCoord(0)} ${this.getYCoord(this.initialTotal())}`,
+        ...median.map((value, index) => `L ${this.getXCoord(index)} ${this.getYCoord(value)}`)
+      ].join(' '),
+      percentiles: [
+        `M ${this.getXCoord(0)} ${this.getYCoord(this.initialTotal())}`,
+        ...lower.map((value, index) => `L ${this.getXCoord(index)} ${this.getYCoord(value)}`),
+        ...upper.map((value, index) => `L ${this.getXCoord(index)} ${this.getYCoord(value)}`).reverse(),
+        'Z'
+      ].join(' ')
+    };
+  });
 
-    const yInterval = maxVisibleAmount > 5000000000 ? 1000000000
-      : maxVisibleAmount > 2500000000 ? 500000000
-      : maxVisibleAmount > 1000000000 ? 250000000
-      : maxVisibleAmount > 500000000 ? 100000000
-      : maxVisibleAmount > 250000000 ? 50000000
-      : maxVisibleAmount > 100000000 ? 25000000
-      : maxVisibleAmount > 50000000 ? 10000000
-      : maxVisibleAmount > 25000000 ? 5000000
-      : maxVisibleAmount > 10000000 ? 2500000
-      : maxVisibleAmount > 5000000 ? 1000000
-      : maxVisibleAmount > 2500000 ? 500000
-      : maxVisibleAmount > 1000000 ? 250000
-      : maxVisibleAmount > 500000 ? 100000
-      : maxVisibleAmount > 250000 ? 50000
-      : maxVisibleAmount > 100000 ? 25000
-      : maxVisibleAmount > 50000 ? 10000
-      : maxVisibleAmount > 25000 ? 5000
-      : maxVisibleAmount > 10000 ? 2500
-      : maxVisibleAmount > 5000 ? 1000
-      : maxVisibleAmount > 2500 ? 500
-      : maxVisibleAmount > 1000 ? 250
-      : maxVisibleAmount > 500 ? 100
-      : maxVisibleAmount > 250 ? 50
-      : maxVisibleAmount > 100 ? 25
-      : maxVisibleAmount > 50 ? 10
-      : maxVisibleAmount > 25 ? 5
-      : 1;
+  getXCoord = (value: number): number => value * this.xUnit();
+  getYCoord = (value: number): number => 100 - value * this.yUnit();
 
-    const yCount = Math.floor(maxVisibleAmount / yInterval);
-    const remainder = maxVisibleAmount - yCount * yInterval;
-    const labelledYPortion = (maxVisibleAmount - remainder) / maxVisibleAmount;
-    const yLabelHeight = labelledYPortion / yCount;
-    this.yLabels.set([...Array(yCount)].map((_, index) => ({
-      value: (index + 1) * yInterval,
-      y: `${100 * (index + 1) * yLabelHeight}%`
-    })));
-
-    this.path.set([
-      `M ${this.getXCoord(0)} ${this.getYCoord(initialTotal)}`,
-      ...lower.map((value, index) => `L ${this.getXCoord(index)} ${this.getYCoord(value)}`),
-      ...upper.map((value, index) => `L ${this.getXCoord(index)} ${this.getYCoord(value)}`).reverse(),
-      'Z'
-    ].join(' '));
-    this.percentilesPath.set([
-      `M ${this.getXCoord(0)} ${this.getYCoord(initialTotal)}`,
-      ...lowerPercentile.map((value, index) => `L ${this.getXCoord(index)} ${this.getYCoord(value)}`),
-      ...upperPercentile.map((value, index) => `L ${this.getXCoord(index)} ${this.getYCoord(value)}`).reverse(),
-      'Z'
-    ].join(' '));
-    this.medianPath.set([
-      `M ${this.getXCoord(0)} ${this.getYCoord(initialTotal)}`,
-      ...median.map((value, index) => `L ${this.getXCoord(index)} ${this.getYCoord(value)}`)
-    ].join(' '));
-  }
-
-  calculateWithdrawal(lastWithdrawal: number, totalPortfolio: number, scheme: DynamicWithdrawalScheme) {
-    const adjustment = scheme.adjustmentPercentage / 100;
-    const target = scheme.targetPercentage / 100;
-    const threshold = scheme.thresholdPercentage / 100;
-    const lowerThreshold = target / (1 + threshold);
-    const upperThreshold = target * (1 + threshold);
-
-    if (totalPortfolio <= lastWithdrawal) {
-      return totalPortfolio;
-    } else if (lastWithdrawal < totalPortfolio * lowerThreshold) {
-      return Math.min(totalPortfolio * target, lastWithdrawal * (1 + adjustment));
-    } else if (lastWithdrawal > totalPortfolio * upperThreshold) {
-      return Math.max(totalPortfolio * target, lastWithdrawal / (1 + adjustment));
-    }
-
-    return lastWithdrawal;
-  }
-
-  getXCoord = (value: number) => this.xOffset + value * this.xUnit;
-  getYCoord = (value: number) => 100 - value * this.yUnit;
-
-
+  private readonly xCount = 13;
 
   private calculateCycle(
     start: Moment,
@@ -332,5 +305,23 @@ export class OverviewComponent {
     }
 
     return data;
+  }
+
+  private calculateWithdrawal(lastWithdrawal: number, totalPortfolio: number, scheme: DynamicWithdrawalScheme) {
+    const adjustment = scheme.adjustmentPercentage / 100;
+    const target = scheme.targetPercentage / 100;
+    const threshold = scheme.thresholdPercentage / 100;
+    const lowerThreshold = target / (1 + threshold);
+    const upperThreshold = target * (1 + threshold);
+
+    if (totalPortfolio <= lastWithdrawal) {
+      return totalPortfolio;
+    } else if (lastWithdrawal < totalPortfolio * lowerThreshold) {
+      return Math.min(totalPortfolio * target, lastWithdrawal * (1 + adjustment));
+    } else if (lastWithdrawal > totalPortfolio * upperThreshold) {
+      return Math.max(totalPortfolio * target, lastWithdrawal / (1 + adjustment));
+    }
+
+    return lastWithdrawal;
   }
 }
